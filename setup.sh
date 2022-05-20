5e3e904c0f57b3a0b9a7ee6d035edd0b0047cc9b
antoninverse <antonin@inv3rse.xyz>
2022-05-20 16:29:48 +0200

    ci: adding convenient setup script

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        echo "Running on Linux"
+        bash ./setup_linux.sh
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "Running on Mac OSX"
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+        # POSIX compatibility layer and Linux environment emulation for Windows
+        echo "Running on Cygwin"
+elif [[ "$OSTYPE" == "msys" ]]; then
+        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+        echo "Running on Msys (MinGW)"
+elif [[ "$OSTYPE" == "win32" ]]; then
+        # I'm not sure this can happen.
+        echo "Running on Windows"
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        echo "Running on FreeBSD"
+else
+        echo "Unknown OS, please setup your environement manually"
+fi
---
+++ setup_linux.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Generate ED25519 ssh key
+echo "Generating ED25519 ssh key"
+ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N ''
+
+# Download and install the latest version of VirtualBox
+echo "Installing VirtualBox"
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
+sudo apt-get update
+sudo apt-get install -y virtualbox-6.1 virtualbox-ext-pack 
+
+# Download and install Vagrant
+echo "Installing Vagrant"
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install vagrant
+
+# Download and install VsCode
+echo "Installing VsCode"
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
